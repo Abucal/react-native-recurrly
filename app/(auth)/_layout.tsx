fad76df0c035bff50a040151ea837607ddeea97f
Abucal <abucalxyrus73@gmail.com>
2026-06-08 09:50:23 +0800

    feat: project setup, nativewind, routing, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,5 @@
-
 import { Stack } from "expo-router";
-import React from "react";
+import React from 'react'
 
 export default function RootLayout() {
   return <Stack screenOptions={{headerShown: false}}/>;
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+const subscriptions = () => {
+  return (
+    <SafeAreaView className="flex-1 items-center bg-background">
+      <Text>subscriptions</Text>
+    </SafeAreaView>
+  );
+};
+
+export default subscriptions;
